12c1c4920dbcc56af9bf650c189a352fb098b43e
arko816 <91142102+arko816@users.noreply.github.com>
2023-09-11 00:11:26 +0530

    Update CreateBooks.jsx

--- src/pages/CreateBooks.jsx
+++ src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://strong-sprinkles-25e59a.netlify.app/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
