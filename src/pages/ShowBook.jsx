53612eecb1ab9d7219dcbb4b44c1e1c6a3a94e5d
arko816 <91142102+arko816@users.noreply.github.com>
2023-09-11 00:17:09 +0530

    Update ShowBook.jsx

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://strong-sprinkles-25e59a.netlify.app/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
